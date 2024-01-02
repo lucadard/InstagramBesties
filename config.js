9086cf28c7c6c30852f6309ac20c718b9a9dd41d
lucadard <lucadardenne@hotmail.com>
2024-01-02 13:57:47 -0300

    improve config file comments

--- config.js
+++ config.js
@@ -1,22 +1,31 @@
 export const config = {
-  // set to true to run the cron job every 30 seconds
-  debug: true,
-  // cron expression to run the cron job every 30 seconds, see https://crontab.guru/ for more info
+  /**
+   * If set to true the cron job will run every 30 seconds
+   */
+  debug: false,
+  /**
+   * Specify the cron expression for the cron job, see https://crontab.guru/ for help
+   */
   cronExpression: "0 */5 * * *", // 5 hours
-  // cronExpression: "0 */10 * * *", // 10 hours
-
-  // time between calls to the Instagram API in seconds
+  /**
+   * Time between calls to the Instagram API in seconds
+   */
   timeBetweenCalls: "1",
-
-  // maximum number of users to add to the close friends list per run
+  /**
+   * Maximum number of users to add to the close friends list per iteration,
+   * this is to avoid Instagram's rate limit, be careful not to set it too high
+   */
   maxBestiesPerRun: 50,
-
-  // file to store the IDs of the users that have already been added to the close friends list
+  /**
+   * File to store the IDs of the users that have already been added to the close friends list
+   */
   filePath: "besties.csv",
 
-  // instagram credentials (optional)
+  /**
+   * Your Instagram credentials
+   */
   credentials: {
-    username: "lucadardenne",
-    password: "vandermirch99",
+    username: "",
+    password: "",
   },
 };
